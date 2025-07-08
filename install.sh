808ea0a2161361cbe82dac059c4f58f78be3665b
莫岳恒 <moyueheng@52tt.com>
2025-07-08 02:32:15 +0000

    build(install): 优化安装脚本并添加新依赖
    - 更新 pip 安装命令，使用 uv 前缀
    - 添加 psutil 依赖
    - 更新部分依赖版本：
      - deepspeed: 0.16.4
      - flash-attn: 2.7.0.post2
      - pynvml: >=12.0.0
      - ray: 2.42.0
      - transformers: 4.48.3
    - 添加 --no-build-isolation 参数以优化安装过程

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-pip3 install accelerate bitsandbytes datasets deepspeed==0.16.4 einops flash-attn==2.7.0.post2 isort jsonlines loralib optimum packaging peft pynvml>=12.0.0 ray[default]==2.42.0 tensorboard torch torchmetrics tqdm transformers==4.48.3 transformers_stream_generator wandb wheel
-pip3 install vllm==0.8.5
-pip3 install "qwen-agent[code_interpreter]"
-pip3 install llama_index bs4 pymilvus infinity_client codetiming tensordict==0.6 omegaconf torchdata==0.10.0 hydra-core easydict dill python-multipart
-pip3 install -e . --no-deps
-pip3 install faiss-gpu-cu12
+uv pip install torch torchmetrics
+uv pip install psutil
+# flash attention and deepspeed
+uv pip install accelerate bitsandbytes datasets "deepspeed==0.16.4" einops "flash-attn==2.7.0.post2" isort jsonlines loralib optimum packaging peft "pynvml>=12.0.0" "ray[default]==2.42.0" tensorboard  tqdm "transformers==4.48.3" transformers_stream_generator wandb wheel --no-build-isolation
+uv pip install "vllm==0.8.5"
+uv pip install "qwen-agent[code_interpreter]"
+uv pip install llama_index bs4 pymilvus infinity_client codetiming "tensordict==0.6" omegaconf "torchdata==0.10.0" hydra-core easydict dill python-multipart
+uv pip install -e . --no-deps
+uv pip install faiss-gpu-cu12
